61684c6a03f1e39f9822ebd848ea49fc95e39713
n0lik <unknown>
2022-03-25 13:45:55 +0200

    Implement gradle version catalog;

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,11 @@ includeBuild("build-logic")
 
 include("app")
 
+/**
+ * All libs' versions has been declared in the project's file (/gradle/libs.versions.toml)
+ * [Gradle version catalog feature](https://docs.gradle.org/current/userguide/platforms.html)
+ * @since Gradle 7.4
+ */
 dependencyResolutionManagement {
     repositories {
         google()
@@ -11,7 +16,6 @@ dependencyResolutionManagement {
         gradlePluginPortal()
     }
 }
-
 pluginManagement {
     repositories {
         google()
@@ -23,8 +27,9 @@ pluginManagement {
         eachPlugin {
             val pluginId = requested.id.id
             when {
+                pluginId.contains("kotlin-dsl") -> useVersion("1.6.10")
                 pluginId.startsWith("org.jetbrains.kotlin") -> useVersion("1.6.10")
-                pluginId.startsWith("com.android.") -> useVersion("7.1.0")
+                pluginId.startsWith("com.android.") -> useVersion("7.1.2")
                 pluginId == "org.jlleitschuh.gradle.ktlint" -> useVersion("10.2.1")
             }
         }
